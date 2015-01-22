94b39a31a3a776a6266d7f536fbde17dab29d5ac
Milos Soskic <artificermil@gmail.com>
2015-01-22 18:56:21 +0000

    first commit

---
+++ generators/app/index.js
@@ -0,0 +1,10 @@
+var yeoman = require('yeoman-generator')
+    util   = require('util');
+// Export the module
+module.exports = Generator;
+
+// Extend the base generator
+function Generator(args, options, config) {
+	yeoman.generators.Base.apply(this, arguments);
+};
+util.inherits(Generator, yeoman.generators.Base);
